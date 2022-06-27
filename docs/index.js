336d1e48135f671e426b500379ff26392470cf8f
Owura Asare <oka217@nyu.edu>
2022-06-27 13:19:52 -0400

    still testing

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,24 @@
+let fs = require('fs')
+
 function myfunct(){
     console.log("this is a function in javascript")
+    
+    fs.readFile('temp.txt', function(err, data){
+        console.log(typeof data)
+        console.log(data)
+    })
 }
+
+// function next_button_click(){
+//     // fs.readFile('temp.txt', function(err, data){
+//     //     console.log(typeof data)
+//     //     console.log(data)
+
+//     // })
+// }
+
+// next_button = getElementById('next-button');
+
+// next_button.addEventListener("click", next_button_click)
 
 document.addEventListener("DOMContentLoaded", myfunct);
