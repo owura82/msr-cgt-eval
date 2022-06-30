902c4070142c3653f560004cf8cccbb2169d312b
Owura Asare <oka217@nyu.edu>
2022-06-30 13:47:14 -0400

    some more changes

--- hossam/index.js
+++ hossam/index.js
@@ -1,37 +1,104 @@
+const coder = "hossam";
+
+function setSampleNumber(sample_number){
+  document.getElementById('sample-num-container').textContent = 'Sample '+sample_number;
+  document.getElementById('sample-number').textContent = sample_number;
+}
+
+function setImages(sample_folder){
+  const rand = Math.floor(Math.random() * 10);
+
+  document.getElementById('bottom-image').src = '../assets/img/evals/'+sample_folder+'/coders/C.png'
+  if (rand % 2){
+    document.getElementById('tl-image').src = '../assets/img/evals/'+sample_folder+'/coders/A.png';
+    document.getElementById('tr-image').src = '../assets/img/evals/'+sample_folder+'/coders/B.png';
+  } else {
+    document.getElementById('tl-image').src = '../assets/img/evals/'+sample_folder+'/coders/B.png';
+    document.getElementById('tr-image').src = '../assets/img/evals/'+sample_folder+'/coders/A.png';
+  }
+  document.getElementById('sample-folder').textContent = sample_folder;
+}
+
+function getSelection(response){
+  if (response === 'tl-button') {
+    return document.getElementById('tl-image').src.endsWith('A.png') ? "A":"B";
+  } else if (response === 'tr-button') {
+    return document.getElementById('tr-image').src.endsWith('A.png') ? "A":"B";
+  } else {
+    return 'C';
+  }
+  
+}
+
+function handleFormSubmit(e){
+  const form = document.getElementById('coder-input');
+  const formData = new FormData(form);
+  
+  const response = formData.get('response');
+ 
+  if (!response){
+    alert('One of the options must be selected');
+  } else {
+    const selection = getSelection(response);
+    const sample_number = document.getElementById('sample-number').textContent;
+    const sample_folder = document.getElementById('sample-folder').textContent;
+
+    const send_selection = new XMLHttpRequest();
+
+    send_selection.onreadystatechange = function (){
+      if (this.readyState == 4 && this.status == 200) {
+
+        const resp = JSON.parse(this.responseText);
+
+        if (resp['sample_folder'] === 'done'){
+          setSampleNumber("No more samples!");
+          return;
+        }
+        setImages(resp['sample_folder'])
+        setSampleNumber(resp['sample_number']);
+      }
+    }
+    console.log('here')
+    const req_body = {
+      coder: coder,
+      sample_folder: sample_folder,
+      sample_number: sample_number,
+      result: selection
+    };
+
+    console.log(JSON.stringify(req_body))
+
+    send_selection.open('POST', 'https://cgt-coder-app.herokuapp.com/store-response');
+    send_selection.setRequestHeader('Content-Type', 'application/json');
+    send_selection.send(JSON.stringify(req_body));
+  }
+  e.preventDefault();
+}
 
 
-function myfunct(){
-    console.log("new log message");
-
+function loadInitialPictures(){
     const folder_request = new XMLHttpRequest();
 
     folder_request.onreadystatechange = function (){
       if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
 
-        //add images to page
-        top_left = document.getElementById('tl-image');
-        top_right = document.getElementById('tr-image');
-        bottom = document.getElementById('bottom-image');
+        const resp = JSON.parse(this.responseText);
 
-        top_left.src = '../assets/img/evals/'+this.responseText+'/coders/A.png'
-        top_right.src = '../assets/img/evals/'+this.responseText+'/coders/B.png'
-        bottom.src = '../assets/img/evals/'+this.responseText+'/coders/C.png'
-        
+        if (resp['sample_folder'] === 'done'){
+          setSampleNumber("No more samples!");
+          return;
+        }
+        setImages(resp['sample_folder'])
+        setSampleNumber(resp['sample_number']);
       }
     }
-
-    folder_request.open('GET', 'https://cgt-coder-app.herokuapp.com/current?coder=hossam');
+    folder_request.open('GET', 'https://cgt-coder-app.herokuapp.com/current?coder='+coder);
     folder_request.send();
+
+    //add event listeners to form
+    const form = document.getElementById('coder-input');
     
+    form.addEventListener('submit', handleFormSubmit);
 }
 
-// function next_button_click(){
-//     
-// }
-
-// next_button = getElementById('next-button');
-
-// next_button.addEventListener("click", next_button_click)
-
-document.addEventListener("DOMContentLoaded", myfunct);
+document.addEventListener("DOMContentLoaded", loadInitialPictures);
